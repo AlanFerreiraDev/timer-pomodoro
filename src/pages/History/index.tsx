555d6a60cd4260cc4b18dbde77f84bda7321be9d
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2022-10-15 13:16:31 -0300

    refactor: Add new History page

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,64 @@
+import { useContext } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+import { CyclesContext } from '../../contexts'
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+export const History = () => {
+  const { cycles } = useContext(CyclesContext)
+
+  function firstLetterUppercase(word: string) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cycles.map((cycle) => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutos</td>
+                  <td>
+                    {firstLetterUppercase(
+                      formatDistanceToNow(cycle.startDate, {
+                        addSuffix: true,
+                        locale: ptBR,
+                      })
+                    )}
+                  </td>
+                  <td>
+                    {cycle.finishedDate && (
+                      <Status statusColor="green">Concluído</Status>
+                    )}
+
+                    {cycle.interruptedDate && (
+                      <Status statusColor="red">Interrompido</Status>
+                    )}
+
+                    {!cycle.finishedDate && !cycle.interruptedDate && (
+                      <Status statusColor="yellow">Em andamento</Status>
+                    )}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
